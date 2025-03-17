8d9c2f3c03ef3f2f5a4a05b3e655d45abc971f88
Aleksey Kish <kish94@mail.ru>
2025-03-17 01:04:08 +0100

    fix frontend docker image

--- docker/frontend/docker-entrypoint.sh
+++ docker/frontend/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
-envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+echo "starting server on port: $PORT";
+envsubst '$PORT' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
